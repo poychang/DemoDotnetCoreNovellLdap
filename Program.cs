3120448e80de408c40f0646bc6596bd7637dede2
Poy Chang <poypost@gmail.com>
2017-10-31 08:36:24 +0800

    初始化 dotnet console 專案

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoDotnetCoreNovellLdap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
