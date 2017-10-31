d5f15839e1b9d49f1d99242376f856f7b561ed78
Poy Chang <poypost@gmail.com>
2017-10-31 08:51:19 +0800

    建立需要管理者帳號的簡單驗證 LDAP 帳戶方法

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Novell.Directory.Ldap;
+using System;
 
 namespace DemoDotnetCoreNovellLdap
 {
@@ -6,7 +7,57 @@ namespace DemoDotnetCoreNovellLdap
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var User = new { username = "tesla", password = "password" };
+
+            var simpleAuthResult = SimpleLdapAuth(User.username, User.password);
+            Console.WriteLine($"LDAP Auth Result: {simpleAuthResult}");
+
+        /// <summary>
+        /// 簡單驗證 LDAP 帳戶
+        /// </summary>
+        /// <remarks>需要管理者帳號的寫法</remarks>
+        /// <param name="username">帳號</param>
+        /// <param name="password">密碼</param>
+        /// <returns>驗證是否成功</returns>
+        public static bool SimpleLdapAuth(string username, string password)
+        {
+            var Host = "ldap.forumsys.com";
+            var BindDN = "cn=read-only-admin,dc=example,dc=com";
+            var BindPassword = "password";
+            var BaseDC = "dc=example,dc=com";
+
+            try
+            {
+                using (var connection = new LdapConnection())
+                {
+                    connection.Connect(Host, LdapConnection.DEFAULT_PORT);
+                    connection.Bind(BindDN, BindPassword);
+
+                    var searchFilter = $"(uid={username})";
+                    var entities = connection.Search(BaseDC, LdapConnection.SCOPE_SUB, searchFilter, null, false);
+
+                    string userDn = null;
+                    while (entities.hasMore())
+                    {
+                        var entity = entities.next();
+                        var account = entity.getAttribute("uid");
+                        if (account != null && account.StringValue == username)
+                        {
+                            userDn = entity.DN;
+                            break;
+                        }
+                    }
+                    if (string.IsNullOrWhiteSpace(userDn)) return false;
+
+                    connection.Bind(userDn, password);
+                    return connection.Bound;
+                }
+            }
+            catch (LdapException e)
+            {
+                throw e;
+            }
+        }
         }
     }
 }
